ec90da56c7123cfb2aa31d4bfc19e6d2881660e8
Conan1985 <Conan1985@users.noreply.github.com>
2017-03-30 16:43:03 -0400

    Update stack-implementation.c

--- Stack implementation/stack-implementation.c
+++ Stack implementation/stack-implementation.c
@@ -3,3 +3,21 @@
  * Discuss the stack data structure. Implement a stack in C using either a linked list or a dynamic array, and justify your decision. 
  * Design the interface to your stack to be complete, consistent, and easy to use.
  */
+
+#include <stdio.h>
+
+int main()
+{
+    typedef struct Element {
+        struct Element *next;
+        void *data;
+    } Element;
+    
+    bool createStack (Element **stack) {
+        *stack = NULL;
+        return true;
+    }
+    
+    return 0;
+}
+
