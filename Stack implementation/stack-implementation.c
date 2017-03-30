fb5f00948f223a2d338d3d068536c7333a5f24e3
Conan1985 <Conan1985@users.noreply.github.com>
2017-03-30 16:17:58 -0400

    Create stack-implementation.c

---
+++ Stack implementation/stack-implementation.c
@@ -0,0 +1,5 @@
+/*
+ * PROBLEM
+ * Discuss the stack data structure. Implement a stack in C using either a linked list or a dynamic array, and justify your decision. 
+ * Design the interface to your stack to be complete, consistent, and easy to use.
+ */
