ad7c868c0515e2e8f67cb56c610a6341a258299d
Conan1985 <Conan1985@users.noreply.github.com>
2017-04-05 14:35:56 -0400

    Add the problem of 'Bugs in removeHead'.

--- Ch4-Linked-Lists/linked-lists.c
+++ Ch4-Linked-Lists/linked-lists.c
@@ -1,9 +1,3 @@
-/*
- * PROBLEM
- * Discuss the stack data structure. Implement a stack in C using either a linked list or a dynamic array, and justify your decision.
- * Design the interface to your stack to be complete, consistent, and easy to use.
- */
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -13,6 +7,13 @@ typedef struct Element {
     void *data;
 } Element;
 
+/*
+ * PROBLEM
+ * Discuss the stack data structure. Implement a stack in C using either a linked list or a dynamic array, and justify your decision.
+ * Design the interface to your stack to be complete, consistent, and easy to use.
+ * 
+ * The solution contains the following push, pop, createStack, and deleteStack functions.
+ */
 bool push (Element **stack, void *data){
     Element *elem = malloc(sizeof(Element));
     if (!elem) return false;
@@ -44,6 +45,23 @@ bool deleteStack ( Element **stack ){
         *stack = next;
     }
     return true;
+}
+
+/* 
+ * PROBLEM
+ * Find and fix the bugs in the following C function that is supposed to remove the head element from a singly linked list:
+ * void removeHead (ListElement *head) {
+ *   free(head); // Line 1
+ *   head = head->next; // Line 2
+ * }
+ */
+void removeHead (Element **head) {
+    Element *temp;
+    if (head && *head) {
+        temp = (*head)->next;
+        free (*head);
+        *head = temp;
+    }
 }
 
 void main() {
