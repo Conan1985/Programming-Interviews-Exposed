175eb300043b6bc89fa3db8652f5b5e722580927
Conan1985 <Conan1985@users.noreply.github.com>
2017-04-05 16:44:32 -0400

    Add 'Mth-to-Last Element of a Linked List' Problem

--- Ch4-Linked-Lists/linked-lists.c
+++ Ch4-Linked-Lists/linked-lists.c
@@ -64,5 +64,32 @@ void removeHead (Element **head) {
     }
 }
 
+/*
+ * PROBLEM
+ * Given a singly linked list, devise a tim- and space-efficient algorithm to find the mth-to-last element of the list. 
+ * Implement your algorithm, taking care to handle relevant error conditions. Define th to last such that when m=0 the 
+ * last element of the list is returned.
+ */
+Element *findMthToEndElement (Element *head, int m) {
+    Element *current, *mBehind;
+    int i;
+    if (!head) {
+        return NULL;
+    }
+    current = head;
+    for (i=0;i<m;i++) {
+        current = current->next;
+        if (!(current->next)) {
+            return NULL;
+        }
+    }
+    mBehind = head;
+    while(!(current->next)) {
+        current = current->next;
+        mBehind = mBehind->next;
+    }
+    return mBehind;
+}
+ 
 void main() {
 }
