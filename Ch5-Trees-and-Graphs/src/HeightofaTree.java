c29125e24183969f35050a0c4a7314875d98feb1
Conan1985 <youweilu@Youweis-MacBook-Pro.local>
2017-04-09 08:40:21 -0400

    Add problem description of height of tree.

--- Ch5-Trees-and-Graphs/src/HeightofaTree.java
+++ Ch5-Trees-and-Graphs/src/HeightofaTree.java
@@ -1,5 +1,8 @@
 /**
- * Created by youweilu on 4/9/17.
+ * PROBLEM
+ * The height of a tree (binary or not) is defined to be the maximum distance from the root node to any leaf node. The
+ * tree in Figure 5-2, for example, has a height of 4 because the path from A to F, G, or H involves four nodes. Write
+ * a function to calculate the height of an rbitrary binary tree.
  */
 public class HeightofaTree{
     private static int treeHeight(Node n){
