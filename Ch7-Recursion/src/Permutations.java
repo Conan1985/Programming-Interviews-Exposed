8a140af1540de1fb032b96802f8eafd2d8742e2f
Conan1985 <youweilu@Youweis-MacBook-Pro.local>
2017-04-17 19:53:01 -0400

    Permutations.

---
+++ Ch7-Recursion/src/Permutations.java
@@ -0,0 +1,30 @@
+/**
+ * PROBLEM
+ * Implement a routine that prints all possible orderings of the characters in a string. In other words, print all
+ * permutations that use all the characters from the original string.
+ */
+public class Permutations {
+    private boolean[] used;
+    private StringBuilder out = new StringBuilder();
+    private final String in;
+
+    public Permutations (final String str) {
+        in = str;
+        used = new boolean[in.length()];
+    }
+
+    private void permute() {
+        if (out.length() == in.length()) {
+            System.out.print(out);
+            return;
+        }
+        for (int i =0; i<in.length(); ++i) {
+            if (used[i]) continue;
+            out.append(in.charAt(i));
+            used[i] = true;
+            permute();
+            used[i] = false;
+            out.setLength(out.length()-1);
+        }
+    }
+}
